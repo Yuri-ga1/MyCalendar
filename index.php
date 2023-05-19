c07966a445b41b47aa431f8360ae0436dc0dcddb
Yuri-ga1 <regota09@gmail.com>
2023-05-19 15:26:26 +0800

    date sort

--- index.php
+++ index.php
@@ -1,62 +1,62 @@
 <html>
     <head>
         <link rel="stylesheet" href="css.css" type="text/css"/>
+        <script src="script.js"></script>
+        <script src="today.php"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     </head>
     <body>
         <div class='active_tasks'>
             <div class='task_filter'>
-                <input name="date_filter" type="radio" value="today" checked/>сегодня
-                <input name="date_filter" type="radio" value="tomorrow"/>завтра
-                <input name="date_filter" type="radio" value="thisWeek"/>на эту неделю
-                <input name="date_filter" type="radio" value="nextWeek"/>на след неделю
+                <form>
+                    <input id='date_filter_id1' name="date_filter" type="radio" value="today" checked/>
+                    <label for='date_filter_id1'>сегодня</label>
+
+                    <input id='date_filter_id2' name="date_filter" value="tomorrow" type="radio"/>
+                    <label for='date_filter_id2'>завтра</label>
+
+                    <input id='date_filter_id3' name="date_filter" value="thisweek" type="radio"/>
+                    <label for='date_filter_id3'>на эту неделю</label>
+
+                    <input id='date_filter_id4' name="date_filter" value="nextweek" type="radio"/>
+                    <label for='date_filter_id4'>на след неделю</label>
+                </form>
             </div>
-            <table class='task_table'>
-                <tr class='tablehead'>
-                    <td class='left_padding'>Тип</td>
-                    <td class='left_padding'>Задача</td>
-                    <td class='left_padding'>Место</td>
-                    <td class='left_padding'>Дата</td>
-                </tr>
-                <?php
-                    $dbname = 'notes_db';
-                    $username = 'root';
-                    $password ='';
-                    $table_name = 'tasks';
-                    $dbo = new PDO('mysql:host=localhost;dbname=' . $dbname, $username, $password);
 
-                    $p = 1;
-                    foreach ($dbo->query("SELECT * FROM `$table_name`;") as $row)
-                    {
-                        if ($p % 2 === 1){
-                            echo '<tr class=\'color_bg\'>';
-                        }else{
-                            echo '<tr>';
+            <div id="fetch">
+            </div>
+            <script>
+                $(document).ready(function() {
+                    $('input[name="date_filter"]').change(function() {
+                        if ($(this).is(':checked')) {
+                            var selectedOption = $(this).val();
+
+                            $.ajax({
+                                url: 'sort.php',
+                                type: 'GET',
+                                data: { option: selectedOption },
+                                success: function(response) {
+                                    $('#fetch').html(response);
+                                }
+                            });
                         }
-                        $p++;
-
-                        $type = $row['type_id'];
-                        foreach ($dbo->query("SELECT `name` FROM `type` WHERE `id` = $type LIMIT 1;") as $type1){
-                            $type = $type1['name'];
-                        }
-
-                        echo '<td class=\'left_padding\'>',$type ,'</td>';
-                        echo '<td class=\'left_padding\'>',$row['name'] ,'</td>';
-                        echo '<td class=\'left_padding\'>',$row['place'] ,'</td>';
-                        echo '<td class=\'left_padding\'>',$row['date'] ,'</td>';
-                        echo '</tr>';
-                    }
-                ?>
-            </table>
+                    });
+                });
+            </script>
         </div>
 
         <div class="create_note">
             <form action="php.php" method="post">
                 <p><input name="theme" type="text" placeholder="Theme"/></p>
 
-                <p><input name="type_name" type="radio" value="Meeting"/>Meeting
-                <input name="type_name" type="radio" value="Call"/>Call
-                <input name="type_name" type="radio" value="Conference"/>Conference
-                <input name="type_name" type="radio" value="Work"/>Work</p>
+                <p><input id='meeting' name="type_name" type="radio" value="Meeting"/>
+                <label for='meeting'>Meeting</label>
+                <input id='call' name="type_name" type="radio" value="Call"/>
+                <label for='call'>Call</label>
+                <input id='conference' name="type_name" type="radio" value="Conference"/>
+                <label for='conference'>Conference</label>
+                <input id='work' name="type_name" type="radio" value="Work"/>
+                <label for='work'>Work</label></p>
 
                 <p><input name="place" type="text" placeholder="Place" defaultValue=""/></p>
 
---
+++ sort.php
@@ -0,0 +1,37 @@
+<?php
+    $dbname = 'notes_db';
+    $username = 'root';
+    $password ='';
+    $table_name = 'tasks';
+    $dbo = new PDO('mysql:host=localhost;dbname=' . $dbname, $username, $password);
+    $p = 1;
+    $html = '<table class=\'task_table\'><tr class=\'tablehead\'><td class=\'left_padding\'>Тип</td><td class=\'left_padding\'>Задача</td><td class=\'left_padding\'>Место</td><td class=\'left_padding\'>Дата</td></tr>';
+
+    $range = [
+        'today' => ' WHERE CURDATE() = `date`',
+        'tomorrow' => ' WHERE CURDATE()+1 = `date`',
+        'thisweek' => ' WHERE DAYOFYEAR(`date`) - DAYOFYEAR(CURTIME()) BETWEEN 0 AND 7',
+        'nextweek' => ' WHERE DAYOFYEAR(`date`) - DAYOFYEAR(CURTIME()) BETWEEN 7 AND 14'
+    ]; 
+    $sort = $range[$_GET['option']];
+    foreach ($dbo->query("SELECT * FROM `$table_name` $sort;") as $row)
+    {
+        if ($p % 2 === 1){
+            $html .= '<tr class=\'color_bg\'>';
+        }else{
+            $html .= '<tr>';
+        }
+        $p++;
+        $type = $row['type_id'];
+        foreach ($dbo->query("SELECT `name` FROM `type` WHERE `id` = $type LIMIT 1;") as $type1){
+            $type = $type1['name'];
+        }
+        $html .= '<td class=\'left_padding\'>'. $type .'</td>';
+        $html .= '<td class=\'left_padding\'>'. $row['name'] .'</td>';
+        $html .= '<td class=\'left_padding\'>'. $row['place'] .'</td>';
+        $html .= '<td class=\'left_padding\'>'. $row['date'] .'</td>';
+        $html .= '</tr>';
+    }
+
+    echo $html. '</table>';
+?>
